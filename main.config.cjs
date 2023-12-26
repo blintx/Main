993b1b61d012fc702452ea67f25ce7750cfbf394
HVCsano <55344602+HVCsano@users.noreply.github.com>
2023-12-26 14:07:37 +0100

    autom. fix

--- main.config.cjs
+++ main.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'ampix-main',
-            script: './dist/index.js',
+            script: './src/index.js',
             // instances: 4,
             // exec_mode: 'cluster',
             watch: false,
