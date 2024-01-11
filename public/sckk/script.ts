8db43373f587f7d3cb839f8a6509738057b3450f
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-01-11 19:18:54 +0100

    vonatós fix

--- public/sckk/script.ts
+++ public/sckk/script.ts
@@ -212,18 +212,6 @@ let doneReturnCount = 0;
 function doneReturn(vontatos: boolean) {
 	doneReturnCount++;
 	if (vontatos && 2 > dates.length) {
-		if (doneReturnCount === dates.length + 1) {
-			for (const manas in fo) {
-				if (manas !== "Összesen") {
-					handleReturn(manas);
-				} else {
-					if (dates.length > 1) {
-						handleReturn(manas);
-					}
-				}
-			}
-		}
-	} else {
 		if (doneReturnCount === dates.length) {
 			for (const manas in fo) {
 				if (manas !== "Összesen") {
